2357fb29d35e982a0dcace0c2a69154c0d0a83de
Grzrzegorzrz <wnguye4@uwo.ca>
2025-02-03 09:57:23 -0500

    Fixed issue with after

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p', builtin.git_files, {}) -- not sure what I was doing here, let's keep it broken lmao
 vim.keymap.set('n', '<leader>fe', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- vim.keymap.set('n', '<C-p', builtin.git_files, {}) -- not sure what I was doing here, let's keep it broken lmao
--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --vim.env.PATH = vim.env.PATH .. '/usr/lib/jdt-language-server-latest/bin'
 
-require("config.lazy")
 require("config.remap")
+require("config.lazy")
 require("config.ascii")
 
 -- hyprlang support
@@ -15,43 +15,32 @@ vim.opt.background = "dark"
 vim.g["gruvbox_material_background"] = "material"
 vim.g["gruvbox_material_colors_override"] = {fg0 = {'#daccad', '234'}}
 vim.cmd([[colorscheme gruvbox-material]])
---vim.cmd([[colorscheme gruvbox]])
 
 -- enables line numbering
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.iskeyword:remove("_") -- makes "_" a word delimiter
-
 -- tab to 2 spaces
+-- TODO: make tabbing language dependent
 vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
 
+vim.opt.iskeyword:remove("_") -- makes "_" a word delimiter
 vim.opt.smartindent = true
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-
-vim.opt.hlsearch = true
+vim.opt.hlsearch = true -- <C-l> to clear highlighting
 vim.opt.incsearch = true
-
 vim.opt.scrolloff = 10
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
-
 vim.opt.hidden = true
-
 vim.opt.cmdheight = 0  -- hides commandline
 
 -- nvim-tree
-
--- OR setup with some options
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
@@ -91,10 +80,22 @@ require("nvim-tree").setup({
 require("config.colours")
 
 -- quickscope
---vim.g["qs_highlight_on_keys"] = {'f', 'F', 't', 'T'}
 vim.g["qs_buftype_blacklist"] = {'terminal', 'nofile'}
 vim.g["qs_filetype_blacklist"] = {'text'}
 vim.g['qs_delay'] = 0
+
 -- quickscope highlight colours
-vim.api.nvim_set_hl(0, 'QuickScopePrimary', { fg = '#B0E0E6', underline=true, ctermfg=155, cterm = {bold=true} })
-vim.api.nvim_set_hl(0, 'QuickScopeSecondary', { fg = '#e7c3fe', underline=true, ctermfg=155, cterm = {bold=true} })
+vim.api.nvim_set_hl(0, 'QuickScopePrimary', {
+  fg = '#B0E0E6',
+  underline=true,
+  ctermfg=155,
+  cterm = {bold=true}
+})
+vim.api.nvim_set_hl(0,
+'QuickScopeSecondary', {
+  fg = '#e7c3fe',
+  underline=true,
+  ctermfg=155,
+  cterm = {bold=true}
+})
+
