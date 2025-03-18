424c3d12404e8fc87d885374027589e13e004816
Grzrzegorzrz <wnguye4@uwo.ca>
2025-03-18 07:52:27 -0400

    Added img-clip

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -27,7 +27,19 @@ return {
       },
       sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_b = {'branch', 'diff', 'diagnostics',
+        {'macro',
+          fmt = function()
+            local reg = vim.fn.reg_recording()
+            if reg ~= "" then
+              return "Recording @" .. reg
+            end
+            return nil
+          end,
+          color = { fg = "#ff9e64" },
+          draw_empty = false,
+        }},
+
         lualine_c = {'filename'},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
