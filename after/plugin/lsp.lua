f3dd13cdde08283b69a31b101b0c9ffb1bbb82e5
Grzrzegorzrz <wnguye4@uwo.ca>
2025-03-22 10:10:42 -0400

    LSPs + rest.nvim

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,20 +1,36 @@
--- lsp_attach is where you enable features that only work
--- if there is a language server active in the file
-local lsp_attach = function(client, bufnr)
-  local opts = {buffer = bufnr}
+-- Reserve a space in the gutter
+-- This will avoid an annoying layout shift in the screen
+vim.opt.signcolumn = 'yes'
 
-  vim.keymap.set('n', '<leader>bb', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts) vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  --vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
-  vim.keymap.set("n", "<leader>ciw", vim.lsp.buf.rename, {})
-end
+-- Add cmp_nvim_lsp capabilities settings to lspconfig
+-- This should be executed before you configure any language server
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- This is where you enable features that only work
+-- if there is a language server active in the file
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', '<leader>bb', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set("n", "<leader>ciw", vim.lsp.buf.rename, {})
+  end,
+})
 
 require('lsp-zero').extend_lspconfig({
   sign_text = true,
@@ -49,10 +65,10 @@ require('lspconfig').lua_ls.setup{
   },
 }
 
-require'lspconfig'.jdtls.setup{}
-require'lspconfig'.vtsls.setup{}
-require'lspconfig'.arduino_language_server.setup{}
-require'lspconfig'.omnisharp.setup{
+require('lspconfig').jdtls.setup{}
+require('lspconfig').vtsls.setup{}
+require('lspconfig').arduino_language_server.setup{}
+require('lspconfig').omnisharp.setup{
   cmd = { "dotnet", "/lib64/omnisharp/OmniSharp.dll" },
   --on_attach = lsp_attach
   settings = {
---
+++ lua/plugins/image.lua
@@ -0,0 +1,8 @@
+return {
+  '3rd/image.nvim',
+  require('image').setup(),
+  -- require('image').setup({
+  --   -- backend = "ueberzug",
+  --   -- processor = "magick_cli",
+  -- })
+}
