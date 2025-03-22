f4034aa11b27ba8aa6f8a86221b528cc03db9257
Grzrzegorzrz <wnguye4@uwo.ca>
2025-03-22 10:38:09 -0400

    Fixed image.nvim and luarocks.nvim interaction

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,8 +1,8 @@
 return {
   '3rd/image.nvim',
-  require('image').setup(),
-  -- require('image').setup({
-  --   -- backend = "ueberzug",
-  --   -- processor = "magick_cli",
-  -- })
+  dependencies = { "luarocks.nvim" },
+  opts = {
+    -- backend = "ueberzug",
+    -- processor = "magick_cli",
+  }
 }
