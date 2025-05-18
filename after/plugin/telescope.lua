f32488cc608d889bf816b046fbf607df51b94686
Grzrzegorzrz <wnguye4@uwo.ca>
2025-05-18 00:54:15 -0400

    Mapping Changes + Refactoring
    Yazi is now <leader>y instead of <leader>o. Minor comment changes and
    redundancy removals
    
    to be squashed

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,5 +5,3 @@ vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fe', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-
--- vim.keymap.set('n', '<C-p', builtin.git_files, {}) -- not sure what I was doing here, let's keep it broken lmao
