97d7985fdb7b143513856dbed7d97578d8558fe8
Grzrzegorzrz <wnguye4@uwo.ca>
2025-06-21 12:56:43 -0400

    Prettier Support
    squash

--- init.lua
+++ init.lua
@@ -35,7 +35,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.hlsearch = true -- <C-l> to clear highlighting
 vim.opt.incsearch = true
-vim.opt.scrolloff = 0
+vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 vim.opt.hidden = true
@@ -43,6 +43,8 @@ vim.opt.cmdheight = 0  -- 0 hides commandline
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.diagnostic.config({ virtual_text = true })
 
 -- misc plugin setups
 require('Comment').setup()
