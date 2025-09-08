8a371f3a8d7e5e5951262b2711fcd85e1b909031
Grzrzegorzrz <wnguye4@uwo.ca>
2025-09-08 08:47:15 -0400

    Feat: telescope grep refactor

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,15 +1,12 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-/>', builtin.find_files, {})
-
-vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 vim.keymap.set('n', '<C-.>', builtin.oldfiles, {})
 
-vim.keymap.set('n', '<leader>fe', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-
 vim.keymap.set('n', '<C-\'>', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("Grep CWD > ") });
+end)
+vim.keymap.set('n', '<A-\'>', function()
+  local current_dir = vim.fn.expand('%:p:h')  -- Get the directory of the current file
+  builtin.grep_string({ search = vim.fn.input("Grep Local > "), cwd = current_dir });
 end)
