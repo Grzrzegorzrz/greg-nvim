5ba624c33acc9f479f95c2785210d615ef135ec9
Grzrzegorzrz <wnguye4@uwo.ca>
2025-10-03 09:36:01 -0400

    autoclose.lua behaviour change

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -12,12 +12,12 @@ return {
         ["]"] = { escape = true, close = false, pair = "[]" },
         ["}"] = { escape = true, close = false, pair = "{}" },
 
-        ['"'] = { escape = true, close = true, pair = '""' },
-        ["'"] = { escape = true, close = true, pair = "''" },
-        ["`"] = { escape = true, close = true, pair = "``" },
+        ['"'] = { escape = true, close = true, pair = '""', disable_command_mode = true },
+        ["'"] = { escape = true, close = true, pair = "''", disable_command_mode = true },
+        ["`"] = { escape = true, close = true, pair = "``", disable_command_mode = true },
       },
       options = {
-        disabled_filetypes = { "text" },
+        disabled_filetypes = { "text", "markdown" },
         disable_when_touch = false,
         touch_regex = "[%w(%[{]",
         pair_spaces = false,
