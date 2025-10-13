6e2de6b040ab338e5e3a461ffa63f7e586f4045a
Grzrzegorzrz <wnguye4@uwo.ca>
2025-10-12 21:46:02 -0400

    virtual editing for visual block

--- init.lua
+++ init.lua
@@ -47,6 +47,7 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 vim.opt.hidden = true
 vim.opt.cmdheight = 0  -- 0 hides commandline
+vim.opt.ve = 'block'
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
