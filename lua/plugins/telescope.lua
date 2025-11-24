fb35cf121664564ff688fb434218c5c17e041846
Grzrzegorzrz <wnguye4@uwo.ca>
2025-11-24 12:46:55 -0500

    refactor telescope.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,24 @@
--- plugins/telescope.lua:
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "debugloop/telescope-undo.nvim",
+  },
+  config = function()
+    require("telescope").setup({
+      -- the rest of your telescope config goes here
+      extensions = {
+        undo = {
+          -- telescope-undo.nvim config, see below
+          mappings = {
+            ["<C-r>"] = require("telescope-undo.actions").restore,
+          }
+        },
+        -- other extensions:
+        -- file_browser = { ... }
+      },
+    })
+    require("telescope").load_extension("undo")
+    vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+  end,
 }
