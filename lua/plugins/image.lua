e3966e3e5983839ed1d7548595e3996503b6cbaf
Grzrzegorzrz <wnguye4@uwo.ca>
2025-12-13 19:03:50 -0500

    minor max_height change

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -3,6 +3,9 @@ return {
   ---@type snacks.Config
   opts = {
     image = {
+      doc = {
+        max_height = 30,
+      }
       -- your image configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
