03a60bafcf3a902b92df6729813037af75b6c43a
Grzrzegorzrz <wnguye4@uwo.ca>
2026-02-23 12:48:36 -0500

    migrate surround.lua v3 -> v4

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -4,14 +4,21 @@ return {
     event = "VeryLazy",
     config = function()
       require("nvim-surround").setup({
-        keymaps = {
-          visual = "<leader>s",
-          visual_line = "<leader>S",
-          normal = "<leader>s",
-          normal_cur = "<leader>ss",
-          normal_line = "<leader>S",
-          normal_cur_line = "<leader>SS",
-        }
+        -- keymaps = {
+        --   visual = "<leader>s",
+        --   visual_line = "<leader>S",
+        --   normal = "<leader>s",
+        --   normal_cur = "<leader>ss",
+        --   normal_line = "<leader>S",
+        --   normal_cur_line = "<leader>SS",
+        -- }
       })
+
+      vim.keymap.set("v", "<leader>s", "<Plug>(nvim-surround-visual)")
+      vim.keymap.set("v", "<leader>S", "<Plug>(nvim-surround-visual-cur)")
+      vim.keymap.set("n", "<leader>s", "<Plug>(nvim-surround-normal)")
+      vim.keymap.set("n", "<leader>ss", "<Plug>(nvim-surround-normal-cur)")
+      vim.keymap.set("n", "<leader>S", "<Plug>(nvim-surround-normal-line)")
+      vim.keymap.set("n", "<leader>SS", "<Plug>(nvim-surround-normal-curr-line)")
     end
 }
