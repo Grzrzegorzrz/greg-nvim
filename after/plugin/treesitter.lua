6405dac4a73289107416d44897259b1c001c8a48
Grzrzegorzrz <wnguye4@uwo.ca>
2026-04-02 11:10:31 -0400

    migrate to opencode

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -23,11 +23,11 @@ require'nvim-treesitter.configs'.setup {
   },
   incremental_selection = {
     enable = true,
-    keymaps = {
-      init_selection = '<CR>',
-      scope_incremental = '<CR>',
-      node_incremental = '<TAB>',
-      node_decremental = '<S-TAB>',
-    },
+    -- keymaps = {
+    --   init_selection = '<CR>',
+    --   scope_incremental = '<CR>',
+    --   node_incremental = '<TAB>',
+    --   node_decremental = '<S-TAB>',
+    -- },
   },
 }
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,54 +1,66 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  opts = {
-    hints = { enabled = true },
-    -- behaviour = { },
-    -- mappings = { },
-    provider = "openai",
-    selection = {
-      hint_display = "none"
-    },
-    providers = {
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "o3-mini", -- your desired model (or use gpt-4o, etc.)
-        extra_request_body = {
-          temperature = 0.75,
-          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-          reasoning_effort = "high", -- low|medium|high, only used for reasoning models
-        },
-      },
-    },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-  },
+--   "yetone/avante.nvim",
+--   event = "VeryLazy",
+--   version = false, -- Never set this value to "*"! Never!
+--   opts = {
+--     hints = { enabled = true },
+--     behaviour = {
+--       auto_approve_tool_permissions = false,
+--     },
+--     -- mappings = { },
+--     provider = "copilot",
+--     selection = {
+--       hint_display = "none"
+--     },
+--     providers = {
+--       -- openai = {
+--       --   endpoint = "https://api.openai.com/v1",
+--       --   model = "o3-mini", -- your desired model (or use gpt-4o, etc.)
+--       --   extra_request_body = {
+--       --     temperature = 0.75,
+--       --     max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+--       --     reasoning_effort = "high", -- low|medium|high, only used for reasoning models
+--       --   },
+--       -- },
+--       -- copilot = {
+--       --   endpoint = "https://api.githubcopilot.com",
+--       --   model = "claude-sonnet-4-20250514", -- your desired model (or use gpt-4o, etc.)
+--       --   -- extra_request_body = {
+--       --   --   temperature = 0.75,
+--       --   --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+--       --   --   reasoning_effort = "high", -- low|medium|high, only used for reasoning models
+--       --   -- },
+--       -- },
+--     },
+--   },
+--   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--   build = "make",
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     --- The below dependencies are optional,
+--     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+--     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+--     {
+--       -- support for image pasting
+--       "HakonHarnes/img-clip.nvim",
+--       event = "VeryLazy",
+--       opts = {
+--         -- recommended settings
+--         default = {
+--           embed_image_as_base64 = false,
+--           prompt_for_file_name = false,
+--           drag_and_drop = {
+--             insert_mode = true,
+--           },
+--           -- required for Windows users
+--           use_absolute_path = true,
+--         },
+--       },
+--     },
+--   },
 }
