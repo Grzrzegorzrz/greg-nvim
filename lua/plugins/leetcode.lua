5401d2cc444216cd7b58e79ddc27375778013b66
Grzrzegorzrz <wnguye4@uwo.ca>
2026-04-02 11:09:59 -0400

    add leetcode.lua

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     -- include a picker of your choice, see picker section for more details
     "nvim-lua/plenary.nvim",
-    -- "MunifTanjim/nui.nvim",
+    "MunifTanjim/nui.nvim",
   },
   opts = {
     -- configuration goes here
