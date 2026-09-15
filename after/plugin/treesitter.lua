12bb2827df078006d01a8a93fa3226cf2629acd2
Grzrzegorzrz <wnguye4@uwo.ca>
2026-09-15 12:59:50 -0400

    migrate tree-sitter to main branch

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,33 +1,35 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "javascript", "typescript", "java", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "hyprlang" },
+local treesitter = require("nvim-treesitter")
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+treesitter.setup()
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+treesitter.install({
+  "javascript",
+  "typescript",
+  "java",
+  "c",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "markdown",
+  "markdown_inline",
+  "hyprlang",
+})
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "javascript",
+    "typescript",
+    "java",
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+    "hyprlang",
   },
-  incremental_selection = {
-    enable = true,
-    -- keymaps = {
-    --   init_selection = '<CR>',
-    --   scope_incremental = '<CR>',
-    --   node_incremental = '<TAB>',
-    --   node_decremental = '<S-TAB>',
-    -- },
-  },
-}
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
